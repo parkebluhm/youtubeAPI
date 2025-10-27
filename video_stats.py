664783646c1918d14a2718376c0ee1d39e3b1ab3
parkebluhm <parke@240logistics.com>
2025-10-27 18:31:59 -0500

    added env to mask key

---
+++ video_stats.py
@@ -0,0 +1,32 @@
+import os
+import requests
+import json
+
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path = "/Users/parkebluhm/data_projects/youtube_elt/.env")
+
+API_KEY = os.getenv("API_KEY")
+
+CHANNEL_HANDLE = "MrBeast"
+
+def get_playlist_id():
+    try:
+        url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
+
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+
+        channel_items = data["items"][0]
+
+        channel_playlist_id = channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
+        print(channel_playlist_id)
+
+        return channel_playlist_id
+    
+    except requests.exceptions.RequestException as e:
+        raise e
+    
+if __name__ == "__main__":
+    get_playlist_id()
